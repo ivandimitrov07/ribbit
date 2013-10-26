71c36bb58743c03617470079dec8701f6f54c333
ivandimitrov07 <idimitrov07@yahoo.com>
2013-10-26 21:05:36 +0300

    added login, logout, favicon

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   def create
   	user = User.find_by_username params[:username]
   	if user and user.authenticate(params[:password])
-  		session[:user_id] = user.user_id
+  		session[:user_id] = user.id
   		redirect_to root_url, notice: "Logged In"
   	else
   		flash[:error] = "Wrong username or password"
