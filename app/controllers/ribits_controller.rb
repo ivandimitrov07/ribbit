4426e1702b442fdb9d9681bd3d11307ea7b1e8f0
ivandimitrov07 <idimitrov07@yahoo.com>
2013-10-26 21:54:05 +0300

    saving ribbits

--- app/controllers/ribits_controller.rb
+++ app/controllers/ribits_controller.rb
@@ -1,2 +1,11 @@
 class RibitsController < ApplicationController
+
+	def create
+		ribit = Ribit.new(params[:ribit])
+		ribit.user_id = current_user.id
+
+		flash[:error] = "Ribbit is too long!" unless ribit.save
+		
+		redirect_to root_url
+	end
 end
