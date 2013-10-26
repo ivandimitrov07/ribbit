83adc0fbeb88f22e8440ec178cd1fe0516e1c8ce
ivandimitrov07 <idimitrov07@yahoo.com>
2013-10-26 21:32:52 +0300

    created association ribit - user

--- app/models/ribit.rb
+++ app/models/ribit.rb
@@ -1,3 +1,10 @@
 class Ribit < ActiveRecord::Base
   attr_accessible :content, :user_id
+
+  #return the ribits in an array, with the newest ones on top
+  default_scope order: "created_at DESC"
+
+  belongs_to :user
+
+  validate :content, length: { maximum: 140 }
 end
